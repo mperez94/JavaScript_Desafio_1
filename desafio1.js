6a97d314439a3afcaf4b6c9729f5383c0c52bf9f
Mariano Perez <mrafaelperez94@hotmail.com>
2021-09-17 19:10:48 -0300

    Desafio Finalizado

--- desafio1.js
+++ desafio1.js
@@ -1,7 +1,13 @@
+//Simulacion de app calculacion promedio para profesores
+
+alert ('Bievenido a Promedio App')
+
 var nombreUsuario = prompt ('Ingrese su Usuario');
 var password = prompt ('Ingrese su Contraseña');
 
 alert ('Bienvenido Profesor/a ' + nombreUsuario );
+
+//Ingeso de valores
 
 var nombreAlumno = prompt ('Ingrese nombre del Alumno/a');
 
@@ -11,7 +17,9 @@ var notaAlumno2 = prompt ('Valor nota parcial 2');
 
 var notaAlumno3 = prompt ('Valor nota parcial 3');
 
-var promedio = (parseFloat (notaAlumno1) + parseFloat (notaAlumno2) + parseFloat (notaAlumno3))/3;
+//Formula Promedio
+
+var promedio = (parseFloat (notaAlumno1) + parseFloat (notaAlumno2) + parseFloat (notaAlumno3)) / 3;
 
 alert ('El promedio del alumno ' + nombreAlumno + ' es de: ' + promedio);
 
